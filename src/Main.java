46f9ea8d9953065c653966c163ec9f7a85dbd425
AnastasiaBogdanova96 <nastbogdanova96@gmail.com>
2025-02-08 13:36:59 +0300

    corrective homework done

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }else if (clientOS == 0 && clientDeviceYear < 2015) {
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
